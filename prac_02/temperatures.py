941ab5c4efa182f0fa8793880e212b841d738bcb
ZhuWeiyu <weiyu.zhu@my.jcu.edu.au>
2023-11-18 08:42:08 +0800

    new

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -1,26 +1,3 @@
-MENU = """C - Convert Celsius to Fahrenheit
-F - Convert Fahrenheit to Celsius
-Q - Quit"""
-
-
-
-def main():
-    print(MENU)
-    choice = input(">>> ").upper()
-    while choice != "Q":
-        if choice == "C":
-            celsius = float(input("Celsius: "))
-            fahrenheit = calculate_fahrenheit(celsius)
-            print(f"Result: {fahrenheit:.2f} F")
-        elif choice == "F":
-            fahrenheit = float(input("Fahrenheit: "))
-            celsius = calculate_celsius(fahrenheit)
-            print(f"Result:{celsius:.2f} C")
-        else:
-            print("Invalid option")
-        print(MENU)
-        choice = input(">>> ").upper()
-    print("Thank you.")
 
 
 def calculate_fahrenheit(celsius):
